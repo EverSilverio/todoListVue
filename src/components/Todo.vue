7f4ff959e66a9b6da94139a3df963741340fea0f
everton <everton.silverio@dealerup.com.br>
2021-02-12 12:27:24 -0300

    First Commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="tile flex-centered" :class="{checked: todo.checked}">
+        <div class="tile-icon">
+        <div>
+            <i class="icon" :class="todo.checked ? 'icon-check' : 'icon-time'"></i>
+        </div>
+        </div>
+        <div class="tile-content">
+            <div class="tile-subtitle">
+                {{ todo.description }}
+            </div>
+        </div>
+        <div class="tile-action">
+            <button @click="$emit('toggle', todo)" class="btn btn-link">
+                <span v-if="todo.checked">To Do</span>
+                <span v-else>Done!</span>
+            </button>
+            <button @click="$emit('remove', todo)" class="btn btn-link">
+                <span class="text-error">Remove</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.checked {
+    text-decoration: line-through;
+    color: lightgray;
+}
+</style>
---
+++ src/views/kanban/Kanban.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Aqui o Kanban!</div>
+</template>
