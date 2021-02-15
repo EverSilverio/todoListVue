b2118dff51dc8ac578383374b76e40dd6f674152
everton <everton.silverio@dealerup.com.br>
2021-02-15 17:48:17 -0300

    Update TodoList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="container grid-xs py-2">
-    <form @submit.prevent="add(todo)">
-      <div class="input-group">
-        <input
-          type="text"
-          v-model="todo.description"
-          class="form-input"
-          placeholder="New Task"
-        />
-        <button class="btn btn-primary input-group-btn" :class="{ loading }">
-          Add!
-        </button>
-      </div>
-    </form>
+    <div class="container grid-xs py-2">
+        <form @submit.prevent="add(todo)">
+            <div class="input-group">
+                <input
+                    type="text"
+                    v-model="todo.description"
+                    class="form-input"
+                    placeholder="New Task"
+                />
+                <button
+                    class="btn btn-primary input-group-btn"
+                    :class="{ loading }"
+                >
+                    Add!
+                </button>
+            </div>
+        </form>
 
-    <div class="todo-list">
-      <todo
-        v-for="t in todos"
-        :key="t.id"
-        @toggle="toggleTodo"
-        @remove="removeTodo"
-        :todo="t"
-      />
+        <div class="todo-list">
+            <todo
+                v-for="t in todos"
+                :key="t.id"
+                @toggle="toggleTodo"
+                @remove="removeTodo"
+                :todo="t"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Todo from "@/components/Todo";
-import { mapActions, mapState } from 'vuex'
-
+import { mapActions, mapState } from "vuex";
 
 export default {
     name: "app",
@@ -38,26 +40,26 @@ export default {
         return { todo: { checked: false } };
     },
     computed: {
-        ...mapState(['todos', 'loading'])
+        ...mapState(["todos", "loading"]),
     },
     methods: {
-        ...mapActions (['addTodo', 'toggleTodo', 'removeTodo']),
+        ...mapActions(["addTodo", "toggleTodo", "removeTodo"]),
 
         async add(todo) {
-            await this.addTodo(todo)
-            this.todo = { checked: false }
-        }
+            await this.addTodo(todo);
+            this.todo = { checked: false };
+        },
     },
 };
 </script>
 
 <style scoped>
 .img-logo {
-  max-width: 200px;
-  margin: 0 auto;
+    max-width: 200px;
+    margin: 0 auto;
 }
 
 .todo-list {
-  padding-top: 2rem;
+    padding-top: 2rem;
 }
 </style>
